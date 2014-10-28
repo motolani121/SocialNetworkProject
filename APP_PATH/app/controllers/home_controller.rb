ccd2d2debc20ce94734a4c8561999ebffb65c417
skaza <siddharthkaza@hotmail.com>
2014-10-28 18:11:31 -0400

    LOGIN AND HOME PAGE

--- APP_PATH/app/controllers/home_controller.rb
+++ APP_PATH/app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
-  def
-    index
+  def  index
+    if user_signed_in?
+      redirect_to :controller=>'user_home', :action =>'index'
+    end
   end
 end
